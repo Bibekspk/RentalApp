1293731296adc6a75bd2c8dbd724daf174d34acc
Bibekspk <bibeksp7@gmail.com>
2021-03-30 01:07:31 +0545

    adding img to db

--- controller/image.js
+++ controller/image.js
@@ -1,3 +1,4 @@
+const db = require("../database");
 
 
 function singleImg (req,res) {
@@ -18,20 +19,35 @@ function singleImg (req,res) {
 }
 
 function multipleImg (req,res) {
-    // var fileInfo = req.file;
-
-    if (req.files) {
-        res.status(201).json({
-            message: "Image uploaded successfully",
-            url: req.files
-        });
-        
-    } else {
-        res.status(500).json({
-            message: "something went wrong ! ",
-            url: req.file.filename
-        });
+    var fileInfo = req.files;
+    var userId = req.params.userId;
+    var roomId = req.params.roomId;
+    if(fileInfo.length > 0){
+        for(i=0;i<fileInfo.length;i++){
+            const element = fileInfo[i];
+            db.query(`INSERT INTO IMAGE (roomId,userId,image) VALUES(?,?,?)`,[
+                roomId,userId,element.filename
+            ],(error,results)=>{
+                if(error){
+                    res.send({
+                        success: false,
+                        message: "Error occured"
+                    })
+                }
+                res.send({
+                    success: true,
+                    message: "Successfully Added"
+                })
+            })
+        }
     }
+    else{
+        res.send({
+            message: "Please selec images",
+            success: false
+        })
+    }
+     
 }
 
 module.exports ={
