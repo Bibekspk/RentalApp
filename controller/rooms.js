b4b67cb11b4a7d8dc9683cbbc826b971546b1ca2
Bibekspk <bibeksp7@gmail.com>
2021-04-18 11:49:54 +0545

    map for backend

--- controller/rooms.js
+++ controller/rooms.js
@@ -7,11 +7,11 @@ const {getUser} = require('../services/userDao');
 
 exports.addRoom = (req,res) => {
     const id = req.params.userId
-const {roomTitle,roomno,description,address,price,parking,bathroom} = req.body;
+const {roomTitle,roomno,description,address,price,parking,bathroom,latitude,longitude} = req.body;
 console.log(req.body) //storing all the value from form to varialbes
 try{
     db.query("INSERT INTO rooms SET ?", 
-    {userId:id, roomTitle: roomTitle, roomno: roomno, description: description, address: address, price: price, parking: parking, bathroom: bathroom},(error,results) =>{
+    {userId:id, roomTitle: roomTitle, roomno: roomno, description: description, address: address, price: price, parking: parking, bathroom: bathroom,Latitude: latitude, Longitude: longitude},(error,results) =>{
         if(error){
            return res.send({
                 success: false,
