d0ef70dbd14d87b50854450ab309007b97233ff9
Bibekspk <bibeksp7@gmail.com>
2021-04-22 00:58:14 +0545

    changes

--- services/userDao.js
+++ services/userDao.js
@@ -16,18 +16,19 @@ function getUser(id){
 
 function delroombyID(userID){
     return new Promise((resolve,reject)=>{
-        db.query('SELECT thumbImg FROM rooms where userId =? ',[userID],(error,results)=>{
+        db.query('SELECT thumb_Img FROM rooms where userId =? ',[userID],(error,results)=>{
             if(results){
                const image = results.thumb_Img
-                fs.unlink(`uploads/${image}`);
-                db.query('DELETE FROM rooms where userId =? ',[userID],(error,results)=>{
-                    if(error){
-                        resolve(resluts)
-                    }
-                    else{
-                        reject(error)
-                    }
-                })
+                fs.unlink(`uploads/${image}`,(error,results)=>{
+                    db.query('DELETE FROM rooms where userId =? ',[userID],(error,results)=>{
+                        if(error){
+                            resolve(resluts)
+                        }
+                        else{
+                            reject(error)
+                        }
+                    })
+                });
                 resolve(results)
             }
             else{
